7b158189f15e94661df91a59beae912c88f14e18
sawnjordan <sawnjordan@gmail.com>
2023-06-21 13:51:55 +0545

    Added information on readme and edited app.js with middleware

--- Readme.md
+++ Readme.md
@@ -28,3 +28,36 @@ http response code willbe send to the client
         502 => Bad Gateway
         503 => Service unavailable
         504 => Gateway timeout
+
+Middleware executes at diffferent level
+->To manipulate the request before your action call
+->To respond to the client/end the request
+->Call next middleware/action call
+
+    -> Garbage collector
+    -> Handle routes
+
+->Every middleware function accepts 3 parameters(compulsary)
+
+a. Application Level Middleware
+-> app.user(mount)
+or
+expressApplication.method(load) //this is always application level middleware
+app.get("/", (req, res, next)=>{})
+
+b. Routing Level Middleware
+->mounted to express.Router()
+-> app.use()
+
+c. Custom Middlware
+-> We normally mount custom middlwar to the route path
+
+d. Third Party MIddleware
+=> multer ==>multipart/form-data
+
+e. Bultin Middlware
+express.json() => json content
+express.static()
+express.urlencoded()
+
+f. Error Handling Middleware //sabai vanda last ma rakhna parcha
--- src/config/app.js
+++ src/config/app.js
@@ -2,15 +2,36 @@ const express = require("express");
 
 const app = express();
 
-app.post("/register", (request, response) => {
+app.use((req, res, next) => {
+  console.log("I am always called");
+  next();
+});
+
+const authCheck = (req, res, next) => {
+  console.log("I am only called on /me router");
+  next();
+};
+
+const loginMiddleware = (request, response, next) => {
+  console.log("I am called on the /login route");
+  next();
+};
+
+app.post("/register", (request, response, next) => {
   //Business Logic
 
   response.status(200).json({ msg: "User has been registered successfully" });
 });
 
-app.post("/login", (req, res) => {
+app.post("/me", authCheck, (request, response, next) => {
+  //Business Logic
+
+  response.status(200).json({ msg: "User has been registered successfully" });
+});
+
+app.post("/login", loginMiddleware, (request, response, next) => {
   //login process
-  res.json({ msg: "Login Successful" });
+  response.json({ msg: "Login Successful" });
 });
 
 module.exports = app;
