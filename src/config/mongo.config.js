a77088cc5956346bf23dacc9b8a2acb2eb3b2bc0
sawnjordan <sawnjordan@gmail.com>
2023-12-29 11:34:46 +0545

    changed mongodb from local to live

--- src/config/mongo.config.js
+++ src/config/mongo.config.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 
 const mongodbInit = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: process.env.MONGODB_NAME,
-    });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.9zt3ivi.mongodb.net/express?retryWrites=true&w=majority`
+    );
+
+    // await mongoose.connect(process.env.MONGODB_URL, {
+    //   dbName: process.env.MONGODB_NAME,
+    // });
     console.log("DB connection is established.");
   } catch (error) {
     console.log(error);
